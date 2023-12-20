cd4388f765e4053c104cb76691940d23298a880d
Anmol-Maheshwari <anmolmaheshwari0910@gmail.com>
2023-12-21 00:10:06 +0530

    commit

--- display.c
+++ display.c
@@ -1,13 +1,14 @@
 #include<stdio.h>
 void main()
-{ int A[3][3],i,j;
+{                               // to display 3*3 MATRIX 
+  int A[3][3],i,j;
   for(i=0;i<3;i++)
     {for(j=0;j<3;j++)
       { printf("enter the elements into an ARRAY:");
         scanf("%d",&A[i][j]);
       }
     }
-    printf("\n the elements of A:\n");
+    printf("\n the elements of A:\n1");
     for(i=0;i<3;i++)
       { for(j=0;j<3;j++)
         { printf("%d",A[i][j]);
